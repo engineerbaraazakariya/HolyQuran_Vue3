18d24fc796ed9f631718f61657a60bdf7f2c7940
mbzakariya@gmail.com <mbzakariya@gmail.com>
2026-03-12 22:02:58 +0300

    feat: split basic meanings to be from 1 -> 114.js

--- src/views/SurahDetail.vue
+++ src/views/SurahDetail.vue
@@ -98,27 +98,32 @@
                 <div class="flex-1 h-px bg-gray-400 dark:bg-gray-600"></div>
               </div>
 
-
-
-
-
-              <span v-for="(word, index) in ayah.text.split(' ')" :data-ayah-group="ayah.numberInSurah" :key="index"
-                :style="{
-                  fontSize: fontSize + 'px',
-                  fontFamily: fontFamily,
-
-                  color: basicMeaning[surah.number - 1][ayah.numberInSurah - 1][index]?.length > 0 ? '#6363f9' : isDark ? 'white' : 'black',
-                  wordSpacing: '0.25em',
-                  cursor: 'default',
-                }" @touchstart="startLongPress($event, surah.number, ayah.numberInSurah)"
-                @click="basicMeaning[surah.number - 1][ayah.numberInSurah - 1][index]?.length > 0 && setMaany(surah, ayah, index)"
-                @touchend="stopLongPress" @touchmove="stopLongPress"
-                @contextmenu.prevent="startLongPress($event, surah.number, ayah.numberInSurah, 0)"
-                class="relative inline-block mx-1">
-                {{ word }}<span class="!w-2 !max-w-2 !min-w-2 flex" :style="{
-                  backgroundColor: surahVariables[surah.number]?.longPressedAyahNumber === ayah.numberInSurah ? 'green' : !isDark ? 'white' : 'black',
-                }" v-if="index !== ayah.text.split(' ').length - 1"></span>
-              </span>
+<span v-for="(word, index) in ayah.text.split(' ')" 
+      :data-ayah-group="ayah.numberInSurah" 
+      :key="index"
+      :style="{
+        fontSize: fontSize + 'px',
+        fontFamily: fontFamily,
+        color: basicMeaning[0] && basicMeaning[0][ayah.numberInSurah - 1] && basicMeaning[0][ayah.numberInSurah - 1][index]?.length > 0 
+               ? '#6363f9' 
+               : isDark ? 'white' : 'black',
+        wordSpacing: '0.25em',
+        cursor: 'default',
+      }" 
+      @touchstart="startLongPress($event, surah.number, ayah.numberInSurah)"
+      @click="basicMeaning[0] && basicMeaning[0][ayah.numberInSurah - 1] && basicMeaning[0][ayah.numberInSurah - 1][index]?.length > 0 && setMaany(surah, ayah, index)"
+      @touchend="stopLongPress" 
+      @touchmove="stopLongPress"
+      @contextmenu.prevent="startLongPress($event, surah.number, ayah.numberInSurah, 0)"
+      class="relative inline-block mx-1">
+  {{ word }}
+  <span class="!w-2 !max-w-2 !min-w-2 flex" 
+        :style="{
+          backgroundColor: surahVariables[surah.number]?.longPressedAyahNumber === ayah.numberInSurah ? 'green' : !isDark ? 'white' : 'black',
+        }" 
+        v-if="index !== ayah.text.split(' ').length - 1">
+  </span>
+</span>
               <span :data-ayah-group="ayah.numberInSurah" :data-page="ayah.page" :data-hizbQuarter="ayah.hizbQuarter"
                 :data-juz="ayah.juz" :id='"ayah-" + ayah.numberInSurah'
                 class="relative flex justify-center items-center" :style="{
@@ -286,8 +291,6 @@
 
 <script setup>
 
-import basicMeaning from '@/assets/meanings_nested.js'
-
 // Ionic Vue components
 import {
   IonIcon,
@@ -349,6 +352,8 @@ import {
   translationOptions
 } from '@/composables/fonts.ts'
 
+
+const basicMeaning = ref([])
 const headerRef = ref(null)
 let gesture = null
 let startX = 0
@@ -492,6 +497,8 @@ onActivated(async () => {
   await handleRouteChange(route.params.number, route.params.scrollTo);
 });
 
+
+
 const modalType = ref('tafsir') // أو 'translation'
 
 
@@ -542,6 +549,20 @@ const goToSurah = (surahNumber) => {
   localStorage.setItem('lastSurah', surahNumber.toString())
   router.replace({ name: 'SurahDetail', params: { number: surahNumber.toString(), scrollTo: 0.0, isReciting: true } })
 }
+
+
+// دالة لتحميل السورة بناءً على الرقم
+const loadSurahMeanings = async (surahNumber) => {
+  try {
+    // استيراد ديناميكي للملف المطلوب
+    const module = await import(`@/assets/surahs/${surahNumber}.js`)
+    basicMeaning.value = module.default
+    console.log(`✅ تم تحميل سورة ${surahNumber}`,basicMeaning.value)
+  } catch (err) {
+    console.error(err)
+  } 
+  } 
+
 const goToNextSurah = () => {
   const currentSurah = Number(route.params.number);
   if (currentSurah === 114) {
@@ -897,7 +918,7 @@ function setMaany(surah, ayah, index) {
   selectedSurahNumber.value = surah.number;
   selectedAyahNumber.value = ayah.numberInSurah;
   currentMaanyAyah.value = ayah;
-  showCurrentMaany.value = basicMeaning[surah.number - 1][ayah.numberInSurah - 1][index][0];
+  showCurrentMaany.value = basicMeaning.value[0][ayah.numberInSurah - 1][index][0];
 }
 
 onBeforeUnmount(() => {
@@ -989,9 +1010,7 @@ onMounted(async () => {
   if (stored) {
     surahVariables.value = JSON.parse(stored);
   }
-
-  // التعامل مع الحالة الأولية
-  // await handleRouteChange(route.params.number, route.params.scrollTo, route.params.isReciting);
+  await loadSurahMeanings(route.params.number);
 });
 
 async function handleScrollTo(scrollTo, surahNumber) {
